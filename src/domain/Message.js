7061f09c822bb3d5abcd3d8bc40e7adf309cdae7
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-06-06 21:19:59 -0300

    feat(server): add message entity

---
+++ src/domain/Message.js
@@ -0,0 +1,44 @@
+class Message {
+  #id;
+  #username;
+  #text;
+  #createdAt;
+
+  static from({ id, username, text, createdAt }) {
+    const instance = new Message();
+
+    instance.#id = id;
+    instance.#username = username;
+    instance.#text = text;
+    instance.#createdAt = createdAt;
+
+    return instance;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get username() {
+    return this.#username;
+  }
+
+  get text() {
+    return this.#text;
+  }
+
+  get createdAt() {
+    return this.#createdAt;
+  }
+
+  snapshot() {
+    return {
+      id: this.id,
+      username: this.username,
+      text: this.text,
+      createdAt: this.createdAt,
+    };
+  }
+}
+
+module.exports = Message;

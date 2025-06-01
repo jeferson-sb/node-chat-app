3e63795aa9428b8cb519dced573c7e4b058a0957
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2025-06-01 11:00:07 -0300

    chore: fix eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("airbnb-base"),
+
+    plugins: {
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.commonjs,
+            ...globals.node,
+        },
+
+        ecmaVersion: 12,
+        sourceType: "commonjs",
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "no-console": "off",
+        "class-methods-use-this": "off",
+    },
+}]);

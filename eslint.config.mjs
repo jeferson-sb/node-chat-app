4288a5079ed5e646fe2ef24d5cf0316bf3f848d8
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2025-06-02 20:09:58 -0300

    fix: remove airbnb eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,39 @@
-import { defineConfig } from "eslint/config";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: compat.extends("airbnb-base"),
-
+export default defineConfig([
+  {
     plugins: {
-        prettier,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.commonjs,
-            ...globals.node,
-        },
+      globals: {
+        ...globals.commonjs,
+        ...globals.node,
+      },
 
-        ecmaVersion: 12,
-        sourceType: "commonjs",
+      ecmaVersion: 12,
+      sourceType: 'commonjs',
     },
 
     rules: {
-        "prettier/prettier": "error",
-        "no-console": "off",
-        "class-methods-use-this": "off",
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+      'class-methods-use-this': 'off',
     },
-}]);
+  },
+]);
